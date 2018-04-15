31d843fd6072d9fd562e21433a8fcd73b1565865
Thomas Mason <thomasmason@hegemon.local>
2018-04-15 18:44:02 -0400

    fixed some issues

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+"@author: Thomas Mason"
+
 from node import *
 
 class Graph:
@@ -5,18 +7,25 @@ class Graph:
         self.vertices = []
         self.num_leafs = (2 ** n)
         self.num_moves = n
+        self.num_nodes = ()(2 ** (n+1))-1)
+                
+        for i in range(0, self.num_nodes):
+            self.vertices.append(Node())
+        self.vertices[0].string = "START"
+        self.structure()
         
-    
+
     # Constructor functions
     def structure(self):
-        for i in range(0, num_leafs-1):
-            self.assign_adjacents()
+        for i in range(0, self.num_leafs-1):
+            self.assign_adjacents(s)
     
     
     def assign_adjacents(self, node):
-        # Children of node vertices[i] are in:
-        #   vertices[2i + 1]
-        #   vertices[2i + 2]
+        """Children of node vertices[i] are in:
+           vertices[2i + 1]
+           vertices[2i + 2] 
+        """
         
         index = self.vertices.index(node)
         
--- node.py
+++ node.py
@@ -1,12 +1,16 @@
+"@author: Thomas Mason"
+
 import random
 
 class Node:
-    def __init__(self, string, *adjacents):
+    def __init__(self, *adjacents):
         self.color = "WHITE"
         self.string = ""
         self.value = None
         self.max = None
         self.min = None
+        self.maxchild = None
+        self.minchild = None
         self.adjacents = []
         
         # Non-leaf nodes
